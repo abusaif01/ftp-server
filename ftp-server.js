9c8529f686deee41c250224b50d43bb8bb8579ca
Saif <abu.saif01@gmail.com>
2019-04-23 16:04:55 +0600

    v1

--- ftp-server.js
+++ ftp-server.js
@@ -2,7 +2,10 @@ var http = require('http');
 var express = require('express');
 const path = require('path');
 const routes = require('express').Router();
+const fs = require('fs');
 
+const dataFolder = './data/';
+const base = 'http://27.147.170.41:4211/file/';
 // routes.get('*', function (req, res) {
 //   res.sendFile(path.join(__dirname + '/dist/index.html'));
 // });
@@ -18,12 +21,34 @@ const routes = require('express').Router();
 // });
 
 var app = express();
-app.use('/got/',express.static('data'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+
+	var file_list = [];
+
+	fs.readdirSync(dataFolder,{ withFileTypes: true }).forEach(file => {
+
+	  if(file.isFile() && file.name.endsWith('.mkv')){
+	  	file_list.push({name: file.name, dir: file.name, size : (fs.statSync(dataFolder+file.name).size/1024/1024/1024).toFixed(2)});
+	  }
+	  
+	});
+
+    res.render('pages/index',{
+        files: file_list,
+        base : base
+    });
+});
+
+
+app.use('/file/',express.static('data'));
 // app.use('/img',express.static('/home/openup/assets/img'));
 app.use('/', routes);
 
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 7011;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 4211;
 var   ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
